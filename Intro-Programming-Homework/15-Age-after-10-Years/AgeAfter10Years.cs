bfe227f33d80759fb1875d7cf8f47aabbf0897f1
YaneYosifov <happy0hour@gmail.com>
2015-01-15 17:45:53 +0200

    First homework

---
+++ Intro-Programming-Homework/15-Age-after-10-Years/AgeAfter10Years.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class AgeAfter10Years
+{
+    static void Main()
+    {
+        string inputBday = Console.ReadLine();
+        DateTime bday = Convert.ToDateTime(inputBday);
+
+        DateTime now = DateTime.Now;
+        int age = now.Year - bday.Year;
+
+        Console.WriteLine(age);
+        Console.WriteLine(age + 10);
+    }
+}
---
+++ Intro-Programming-Homework/17-Play-with-the-Debugger-in-Visual-Studio/PlayWithTheDebugger.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class PlayWithTheDebugger
+{
+    static void Main()
+    {
+        for (int i = 1; i < 101; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
