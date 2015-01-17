680279785c0ca9a0af2b65906544bffaccfa135d
YaneYosifov <happy0hour@gmail.com>
2015-01-17 23:05:40 +0200

    - Update to second homework - Some resorts and renaming

---
+++ 03-Primitive-Data-Types-and-Variables/08-Isosceles-Triangle/IsoscelesTriangle.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+class IsoscelesTriangle
+{
+    static void Main()
+    {
+        // * Write a program that prints an isosceles triangle of 9 copyright symbols ©, something like this:
+        //    ©
+        //   © ©
+        //  ©   ©
+        // © © © ©
+
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.WriteLine("   \u00A9");
+        Console.WriteLine("  \u00A9 \u00A9");
+        Console.WriteLine(" \u00A9   \u00A9");
+        Console.WriteLine("\u00A9 \u00A9 \u00A9 \u00A9");
+    }
+}
---
+++ 03-Primitive-Data-Types-and-Variables/12-Null-Values-Arithmetic/NullValuesArithmetic.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class NullValuesArithmetic
+{
+    static void Main()
+    {
+        // * Create a program that assigns null values to an integer and to a double variable.
+        // * Try to print these variables at the console.
+        // * Try to add some number or the null literal to these variables and print the result.
+
+        int? intNull = null;
+        double? doubleNull = null;
+        Console.WriteLine(intNull + "\n" + doubleNull);
+
+        intNull = 24;
+        doubleNull = 7;
+        Console.WriteLine(intNull + "\n" + doubleNull);
+    }
+}
