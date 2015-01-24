5ed18aa65926fccd024f799dcc4d46b3a0edcbbb
YaneYosifov <happy0hour@gmail.com>
2015-01-24 03:35:24 +0200

    Homework #5

---
+++ 05-Console-Input-Output/01-Sum-of-3-Numbers/SumOf3Numbers.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class SumOf3Numbers
+{
+    static void Main()
+    {
+        // * Write a program that reads 3 real numbers 
+        //   from the console and prfloats their sum.
+
+        Console.Write("a = ");
+        float a = float.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        float b = float.Parse(Console.ReadLine());
+        Console.Write("c = ");
+        float c = float.Parse(Console.ReadLine());
+
+        Console.WriteLine(a + b + c);
+    }
+}
---
+++ 05-Console-Input-Output/09-Sum-of-n-Numbers/SumOfNNumbers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class SumOfNNumbers
+{
+    static void Main()
+    {
+        // * Write a program that enters a number n and after that 
+        //   enters more n numbers and calculates and prints their sum.
+
+        Console.Write("n = ");
+        int n = int.Parse(Console.ReadLine());
+        double sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += double.Parse(Console.ReadLine());
+        }
+
+        Console.WriteLine(sum);
+    }
+}
