7c9397518f6ea6c460de4e0af003c134b3f51df8
YaneYosifov <happy0hour@gmail.com>
2015-01-27 02:38:24 +0200

    Условни конструкции

---
+++ 06-Conditional-Statements/05-The-Biggest-of-3-Numbers/TheBiggestOf3Numbers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class TheBiggestOf3Numbers
+{
+    static void Main()
+    {
+        // * Write a program that finds the biggest of three numbers.
+
+        Console.Write("a = ");
+        double a = double.Parse(Console.ReadLine());
+        Console.Write("b = ");
+        double b = double.Parse(Console.ReadLine());
+        Console.Write("c = ");
+        double c = double.Parse(Console.ReadLine());
+
+        if (a >= b && a >= c)
+        {
+            Console.WriteLine(a);
+        }
+        else if (b >= a && b >= c)
+        {
+            Console.WriteLine(b);
+        }
+        else
+        {
+            Console.WriteLine(c);
+        }
+    }
+}
