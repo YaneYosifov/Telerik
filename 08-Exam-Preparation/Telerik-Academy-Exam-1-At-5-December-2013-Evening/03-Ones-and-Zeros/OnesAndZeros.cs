d72590c863207478b1b464c0c8793ea72e171159
YaneYosifov <happy0hour@gmail.com>
2015-02-01 06:31:32 +0200

    Exam preparation Solved 4 problems for 8 hours My first preparation

---
+++ 08-Exam-Preparation/Telerik-Academy-Exam-1-At-5-December-2013-Evening/03-Ones-and-Zeros/OnesAndZeros.cs
@@ -0,0 +1,140 @@
+﻿using System;
+class OnesAndZeros
+{
+    static void Main()
+    {
+        long n = long.Parse(Console.ReadLine());
+
+        // long remainder;
+        // string result = string.Empty;
+        // while (n > 0)
+        // {
+        //     remainder = n % 2;
+        //     n /= 2;
+        //     result = remainder.ToString() + result;
+        // }
+        //   
+        // Console.WriteLine(result.PadLeft(16, '0'));
+
+
+        for (int p = 15; p >= 0; p--)
+        {
+            long mask = 1 << p;
+            long bit = (n & mask) >> p;
+
+            if (bit == 0 && p != 0)
+            {
+                Console.Write("###.");
+            }
+            else if (bit == 1 && p != 0)
+            {
+                Console.Write(".#..");
+            }
+            else if (bit == 0 && p == 0)
+            {
+                Console.Write("###");
+            }
+            else if (bit == 1 && p == 0)
+            {
+                Console.Write(".#.");
+            }
+        }
+        Console.WriteLine();
+
+        for (int p = 15; p >= 0; p--)
+        {
+            long mask = 1 << p;
+            long bit = (n & mask) >> p;
+
+            if (bit == 0 && p != 0)
+            {
+                Console.Write("#.#.");
+            }
+            else if (bit == 1 && p != 0)
+            {
+                Console.Write("##..");
+            }
+            else if (bit == 0 && p == 0)
+            {
+                Console.Write("#.#");
+            }
+            else if (bit == 1 && p == 0)
+            {
+                Console.Write("##.");
+            }
+        }
+        Console.WriteLine();
+
+        for (int p = 15; p >= 0; p--)
+        {
+            long mask = 1 << p;
+            long bit = (n & mask) >> p;
+
+            if (bit == 0 && p != 0)
+            {
+                Console.Write("#.#.");
+            }
+            else if (bit == 1 && p != 0)
+            {
+                Console.Write(".#..");
+            }
+            else if (bit == 0 && p == 0)
+            {
+                Console.Write("#.#");
+            }
+            else if (bit == 1 && p == 0)
+            {
+                Console.Write(".#.");
+            }
+        }
+        Console.WriteLine();
+
+        for (int p = 15; p >= 0; p--)
+        {
+            long mask = 1 << p;
+            long bit = (n & mask) >> p;
+
+            if (bit == 0 && p != 0)
+            {
+                Console.Write("#.#.");
+            }
+            else if (bit == 1 && p != 0)
+            {
+                Console.Write(".#..");
+            }
+            else if (bit == 0 && p == 0)
+            {
+                Console.Write("#.#");
+            }
+            else if (bit == 1 && p == 0)
+            {
+                Console.Write(".#.");
+            }
+        }
+        Console.WriteLine();
+
+        for (int p = 15; p >= 0; p--)
+        {
+            long mask = 1 << p;
+            long bit = (n & mask) >> p;
+
+            if (bit == 0 && p != 0)
+            {
+                Console.Write("###.");
+            }
+            else if (bit == 1 && p != 0)
+            {
+                Console.Write("###.");
+            }
+            else if (bit == 0 && p == 0)
+            {
+                Console.Write("###");
+            }
+            else if (bit == 1 && p == 0)
+            {
+                Console.Write("###");
+            }
+        }
+        Console.WriteLine();
+    }
+}
