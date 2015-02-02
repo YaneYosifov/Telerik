aec82a951f1cb87051cdf1fa4db1ab0e3fd83b94
YaneYosifov <happy0hour@gmail.com>
2015-02-02 04:05:27 +0200

    little preparation

---
+++ 08-Exam-Preparation/Telerik-Academy-Exam-1-At-6-December-2013-Evening/01-2-4-8/2-4-8.cs
@@ -0,0 +1,34 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        long a = long.Parse(Console.ReadLine());
+        long b = long.Parse(Console.ReadLine());
+        long c = long.Parse(Console.ReadLine());
+        long r = 0;
+
+        if (b == 2)
+        {
+            r = a % c;
+        }
+        else if (b == 4)
+        {
+            r = a + c;
+        }
+        else if (b == 8)
+        {
+            r = a * c;
+        }
+
+        if (r % 4 == 0)
+        {
+            Console.WriteLine(r / 4);
+        }
+        else
+        {
+            Console.WriteLine(r % 4);
+        }
+        Console.WriteLine(r);
+    }
+}
