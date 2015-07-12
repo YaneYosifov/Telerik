83a0b3544e790e95ebe6c21c2c571752abd81c29
YaneYosifov <happy0hour@gmail.com>
2015-07-12 03:03:19 +0300

    Added homework: Variables, Data, Expressions and Constants

---
+++ High-Quality Code/Homeworks/05. Variables, Data, Expressions and Constants/Class Size in CSharp/Size.cs
@@ -0,0 +1,42 @@
+﻿namespace Size
+{
+    using System;
+
+    public class Size
+    {
+        private double width, height;
+
+        public Size(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Width
+        {
+            get { return this.width; }
+            set { this.width = value; }
+        }
+
+        public double Height
+        {
+            get { return this.height; }
+            set { this.height = value; }
+        }
+
+        public static Size GetRotatedSize(Size size, double angle)
+        {
+            double sinOfAngle = Math.Sin(angle);
+            double cosOfAngle = Math.Cos(angle);
+
+            double sinAbs = Math.Abs(sinOfAngle);
+            double cosAbs = Math.Abs(cosOfAngle);
+
+            double newWidth = (cosAbs * size.Width) + (sinAbs * size.Height);
+            double newHeight = (sinAbs * size.Width) + (cosAbs * size.Height);
+
+            Size newSize = new Size(newWidth, newHeight);
+            return newSize;
+        }
+    }
+}
---
+++ High-Quality Code/Homeworks/05. Variables, Data, Expressions and Constants/Method PrintStatistics in CSharp/Statistics.cs
@@ -0,0 +1,40 @@
+﻿namespace Statistics
+{
+    using System;
+
+    public class Statistics
+    {
+        public void PrintStatistics(double[] numbers, int count)
+        {
+            double max = 0;
+            for (int i = 0; i < count; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+            }
+
+            PrintMax(max);
+
+            double min = max;
+            for (int i = 0; i < count; i++)
+            {
+                if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+            }
+
+            PrintMin(min);
+
+            double sum = 0;
+            for (int i = 0; i < count; i++)
+            {
+                sum += numbers[i];
+            }
+
+            PrintAvg(sum / count);
+        }
+    }
+}
