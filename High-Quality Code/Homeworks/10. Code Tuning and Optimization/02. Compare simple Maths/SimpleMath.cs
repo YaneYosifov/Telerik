4f9eb260ea02110b1ac345ce78103cbdba3f17d9
YaneYosifov <happy0hour@gmail.com>
2015-08-03 11:07:28 +0300

    Added homework: Code Tuning and Optimization

---
+++ High-Quality Code/Homeworks/10. Code Tuning and Optimization/02. Compare simple Maths/SimpleMath.cs
@@ -0,0 +1,94 @@
+﻿namespace MathPerformanceTest
+{
+    using System;
+    using System.Diagnostics;
+
+    public class SimpleMath
+    {
+        public static void Main()
+        {
+            const int INT = 0;
+            ComparePerformance(INT);
+
+            const long LONG = 0L;
+            ComparePerformance(LONG);
+
+            const float FLOAT = 0f;
+            ComparePerformance(FLOAT);
+
+            const double DOUBLE = 0;
+            ComparePerformance(DOUBLE);
+
+            const decimal DECIMAL = 0m;
+            ComparePerformance(DECIMAL);
+        }
+
+        private static void ComparePerformance(dynamic count)
+        {
+            const int ITERATIONS = 100000000;
+
+            Console.Write("{0, -25}", count.GetType() + " ++");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    count++;
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " += 1");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    count += 1;
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " --");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    count--;
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " -= 1");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    count -= 1;
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " *= 1");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    count *= 1;
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " /= 1");
+            DisplayExecutionTime(() =>
+            {
+                for (int i = 0; i < ITERATIONS; i++)
+                {
+                    count /= 1;
+                }
+            });
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
---
+++ High-Quality Code/Homeworks/10. Code Tuning and Optimization/03. Compare advanced Maths/AdvancedMath.cs
@@ -0,0 +1,112 @@
+﻿namespace MathPerformanceTest
+{
+    using System;
+    using System.Diagnostics;
+
+    public class SimpleMath
+    {
+        public static void Main()
+        {
+            const float FLOAT = 1f;
+            ComparePerformance(FLOAT);
+
+            const double DOUBLE = 1;
+            ComparePerformance(DOUBLE);
+
+            const decimal DECIMAL = 1m;
+            ComparePerformance(DECIMAL);
+        }
+
+        private static void ComparePerformance(dynamic count)
+        {
+            const int ITERATIONS = 15000000;
+
+            Console.Write("{0, -25}", count.GetType() + " Sqrt");
+            DisplayExecutionTime(() =>
+            {
+                if (count.GetType() == typeof(decimal))
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += (decimal)Math.Sqrt((double)count);
+                    }
+                }
+                else if (count.GetType() == typeof(float))
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += (float)Math.Sqrt(count);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += Math.Sqrt(count);
+                    }
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " Log");
+            DisplayExecutionTime(() =>
+            {
+                if (count.GetType() == typeof(decimal))
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += (decimal)Math.Log((double)count);
+                    }
+                }
+                else if (count.GetType() == typeof(float))
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += (float)Math.Log(count);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += Math.Log(count);
+                    }
+                }
+            });
+
+            Console.Write("{0, -25}", count.GetType() + " Sin");
+            DisplayExecutionTime(() =>
+            {
+                if (count.GetType() == typeof(decimal))
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += (decimal)Math.Sin((double)count);
+                    }
+                }
+                else if (count.GetType() == typeof(float))
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += (float)Math.Sin(count);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < ITERATIONS; i++)
+                    {
+                        count += Math.Sin(count);
+                    }
+                }
+            });
+        }
+
+        private static void DisplayExecutionTime(Action action)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            action();
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed);
+        }
+    }
+}
