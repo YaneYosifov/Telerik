537b446acfa3b4f4b363948bbbe7fa6fddc0caf2
YaneYosifov <happy0hour@gmail.com>
2015-10-15 18:33:16 +0300

    Code documented

--- Exam/ConsoleWebServer/ConsoleWebServer.Framework/IActionResult.cs
+++ Exam/ConsoleWebServer/ConsoleWebServer.Framework/IActionResult.cs
@@ -1,7 +1,14 @@
 ﻿namespace ConsoleWebServer.Framework
 {
+    /// <summary>
+    /// Interface to implement HTTP response
+    /// </summary>
     public interface IActionResult
     {
+        /// <summary>
+        /// Gets HTTP response
+        /// </summary>
+        /// <returns>Response from the server</returns>
         HttpResponse GetResponse();
     }
 }
--- Exam/ConsoleWebServer/ConsoleWebServer.Framework/IResponseProvider.cs
+++ Exam/ConsoleWebServer/ConsoleWebServer.Framework/IResponseProvider.cs
@@ -1,7 +1,15 @@
 ﻿namespace ConsoleWebServer.Framework
 {
+    /// <summary>
+    /// Interface to implement HTTP response
+    /// </summary>
     public interface IResponseProvider
     {
+        /// <summary>
+        /// Gets HTTP response
+        /// </summary>
+        /// <param name="requestAsString">Request in string format</param>
+        /// <returns>Response from the server</returns>
         HttpResponse GetResponse(string requestAsString);
     }
 }
